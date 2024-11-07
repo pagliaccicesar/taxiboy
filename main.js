e2627854efbb45075f607953c327e5588f63c959
cesar pagliacci <pagliaccicesar@gmail.com>
2024-11-07 20:17:54 -0300

    se cambio scroll mas lento

--- main.js
+++ main.js
@@ -67,13 +67,17 @@ function prevSlide() {
   }
 }*/
 
-
-
-
-
-
-
-
+/***  js de scroll mas lento  ***/
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+  });
+});
 
 
 
